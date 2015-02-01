7b63d06fd324da82213165b6709482fdc4ef78f6
Shane Barringer <shane.barringer@outlook.com>
2015-02-01 17:40:00 -0500

    Adds todo items controller and spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
+  
+
   root to: 'todo_lists#index'
 
-  resources :todo_lists
+  resources :todo_lists do
+    resources :todo_items
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
